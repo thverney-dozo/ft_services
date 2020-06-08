c545c166e5ce130b85aee07388fb7736496eb2c8
Thverney-Dozo <theo7420@hotmail.fr>
2020-06-08 16:10:45 +0200

    start

--- srcs/mariadb-setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';
-GRANT USAGE ON *.* TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
