1e7bd45c91756d16483259e4cd71e9de4be6179e
Thverney-Dozo <theo7420@hotmail.fr>
2020-06-12 18:24:44 +0200

    add wp and fix mysql..

--- thverney/setup.sh
+++ thverney/setup.sh
@@ -11,9 +11,11 @@ minikube addons enable dashboard
 eval $(minikube docker-env)
 docker build -t nginx_alpine srcs/nginx/
 docker build -t mysql_alpine srcs/mysql/
+docker build -t wordpress_alpine srcs/wordpress/
 
 
 kubectl apply -f srcs/nginx-deployment.yaml
 kubectl apply -f srcs/ingress.yaml
 kubectl apply -f srcs/mysql.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress.yaml
