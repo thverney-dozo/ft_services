e49efa6253518c25c00bedc8a49a6d261efc9631
Theo VERNEY <thverney@e1r8p4.42.fr>
2020-07-11 05:00:16 +0200

    mmh

--- thverney/setup.sh
+++ thverney/setup.sh
@@ -4,10 +4,9 @@ minikube delete
 
 minikube config set vm-driver virtualbox
 
-minikube start --cpus=2 --memory 4000
-minikube addons enable ingress
+minikube start --cpus=2 --memory 4000 --disk-size 11000 --extra-config=apiserver.service-node-port-range=1-35000
 minikube addons enable dashboard
-
+minikube addons enable metallb
 printf "✓	Minikube start successful\n"
 
 eval $(minikube docker-env)
@@ -18,8 +17,16 @@ docker build -t wordpress_alpine srcs/wordpress/
 
 printf "✓   All docker build successful\n"
 
+
+
+
+kubectl apply -f srcs/metallb.yaml
+kubectl apply -f srcs/metallb-config.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/nginx-deployment.yaml
-kubectl apply -f srcs/ingress.yaml
+kubectl apply -f srcs/nginx-deployment.yaml
+kubectl apply -f srcs/nginx-deployment.yaml
+kubectl apply -f srcs/nginx-deployment.yaml
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress.yaml
