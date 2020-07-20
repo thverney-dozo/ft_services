7cab95eed198c355aaac0a07f4c92aac5e5b8178
Theo VERNEY <thverney@e1r11p6.42.fr>
2020-07-20 11:09:20 +0200

    mmh

--- thverney/srcs/phpmyadmin/start.sh
+++ thverney/srcs/phpmyadmin/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+mysql -u $PMA_USER -h $PMA_HOST -p$MYSQL_ROOT_PASSWORD
+while [ ?$ != 0] do
+    mysql -u $PMA_USER -h $PMA_HOST -p$MYSQL_ROOT_PASSWORD;
+    sleep 2;
+done
 php -S 0.0.0.0:5000 -t /www/
